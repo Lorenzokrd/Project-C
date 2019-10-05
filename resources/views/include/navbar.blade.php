5cf0bef9d13ff267e7dfe57240ca0fb353b40283
LorenzoKruidenier <LorenzoKruidenier@hotmail.com>
2019-10-06 01:24:55 +0200

    Added hover effect and room for info/tags to restaurant card

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -1,20 +1,19 @@
 
 <head>
-<title></title>
+<title>Alleen de beste restaurants in Rotterdam - Rotterdambezorgd.nl</title>
 
 <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}" >
 <script src="https://kit.fontawesome.com/1cde44e559.js" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-<nav class="navbar navbar-expand-md">
-    <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+<nav class="navbar navbar-expand-md fixed-top">
+    <div class="w-100 order-1 order-md-0">
         <img class="nav-logo" src="{{URL('/images/logo.png')}}">
     </div>
     <div class="mx-auto order-0 md-form mt-0 w-100">
-        <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+        <input class="form-control" type="text" placeholder="Vind restaurant" aria-label="Search">
     </div>
     <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
         <ul class="navbar-nav ml-auto">
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,11 +2,11 @@
 <html>
 @include('include.navbar')
 
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+<div id="carousel" class="carousel slide" data-ride="carousel" data-interval="false">
     <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        <li data-target="#carousel" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel" data-slide-to="1"></li>
+        <li data-target="#carousel" data-slide-to="2"></li>
     </ol>
     <div class="carousel-inner">
         <div class="carousel-item active">
@@ -22,11 +22,11 @@
             <img class="d-block w-100" src="{{URL('/images/steak.png')}}" alt="Third slide">
         </div>
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
     </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
     </a>
@@ -93,75 +93,89 @@
         </div>
         <div class="col-lg-10">
             <div class="row">
-                <div class="col-lg-4 col-sm-6 restaurant-grid-item">
+                <div class="col col-12 col-sm-6 col-lg-4 restaurant-grid-item" onclick="document.location='/#';return false;">
                     <div class="restaurant-card" style="background-image:url('https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500');">
                         <div class="restaurant-name">
                             <p><i class="far fa-star" aria-hidden="true"></i> Burger me</p>
                         </div>
+                        <div class="status restaurant-status-recommended">
+                            <p>Aanbevolen voor jou</p>
+                        </div>
                         <div class="restaurant-info">
-                            <img class="restaurant-score" src="https://cdn.discordapp.com/attachments/523130323618234418/629730192096624650/stars.png">
+                            <img class="restaurant-score" src="https://cdn.discordapp.com/attachments/206957264408412162/630042617047941132/image_1.png">
                             <p class="price"><i class="fas fa-shopping-basket"></i> Min. €20,00</p>
                             <p class="time"><i class="far fa-clock"></i> 40 min</p>
+                            <p class="tags">Burgers, salades, patat</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-sm-6 restaurant-grid-item">
+                <div class="col col-12 col-sm-6 col-lg-4 restaurant-grid-item" onclick="document.location='/#';return false;">
                     <div class="restaurant-card" style="background-image:url('https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&w=1000&q=80');">
                         <div class="restaurant-name">
                             <p><i class="far fa-star" aria-hidden="true"></i> Pannenkoekenhuis Dutch Diner</p>
                         </div>
                         <div class="restaurant-info">
-                            <img class="restaurant-score" src="https://cdn.discordapp.com/attachments/523130323618234418/629730192096624650/stars.png">
+                            <img class="restaurant-score" src="https://cdn.discordapp.com/attachments/206957264408412162/630042617047941132/image_1.png">
                             <p class="price"><i class="fas fa-shopping-basket"></i> Min. €13,00</p>
                             <p class="time"><i class="far fa-clock"></i> 30 min</p>
+                            <p class="tags">Pannenkoeken, poffertjes, ontbijt</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-sm-6 restaurant-grid-item">
+                <div class="col col-12 col-sm-6 col-lg-4 restaurant-grid-item" onclick="document.location='/#';return false;">
                     <div class="restaurant-card" style="background-image:url('https://d1ralsognjng37.cloudfront.net/b3354ed7-9661-4ae5-85bc-9b7414d176a3');">
                         <div class="restaurant-name">
                             <p><i class="far fa-star" aria-hidden="true"></i> Poke Bowls Byns by Han</p>
                         </div>
                         <div class="restaurant-info">
-                            <img class="restaurant-score" src="https://cdn.discordapp.com/attachments/523130323618234418/629730192096624650/stars.png">
+                            <img class="restaurant-score" src="https://cdn.discordapp.com/attachments/206957264408412162/630042617047941132/image_1.png">
                             <p class="price"><i class="fas fa-shopping-basket"></i> Min. €10,00</p>
                             <p class="time"><i class="far fa-clock"></i> 55 min</p>
+                            <p class="tags">Poke bowls, salades, shakes</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-sm-6 restaurant-grid-item">
+                <div class="col col-12 col-sm-6 col-lg-4 restaurant-grid-item" onclick="document.location='/#';return false;">
                     <div class="restaurant-card" style="background-image:url('https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/sirloin_steak.jpg');background-position:center;">
                         <div class="restaurant-name">
                             <p><i class="far fa-star" aria-hidden="true"></i> Steakhouse Pizzeria Bergo</p>
                         </div>
                         <div class="restaurant-info">
-                            <img class="restaurant-score" src="https://cdn.discordapp.com/attachments/523130323618234418/629730192096624650/stars.png">
+                            <img class="restaurant-score" src="https://cdn.discordapp.com/attachments/206957264408412162/630042617047941132/image_1.png">
                             <p class="price"><i class="fas fa-shopping-basket"></i> Min. €11,00</p>
                             <p class="time"><i class="far fa-clock"></i> 45 min</p>
+                            <p class="tags">Pizza, fingerfood</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-sm-6 restaurant-grid-item">
+                <div class="col col-12 col-sm-6 col-lg-4 restaurant-grid-item" onclick="document.location='/#';return false;">
                     <div class="restaurant-card" style="background-image:url('https://www.thuisbezorgd.nl/foodwiki/uploads/2017/05/sushi-3-1080x960.jpg');background-position:center;">
                         <div class="restaurant-name">
                             <p><i class="far fa-star" aria-hidden="true"></i> Sushito | Magna Plaza</p>
                         </div>
                         <div class="restaurant-info">
-                            <img class="restaurant-score" src="https://cdn.discordapp.com/attachments/523130323618234418/629730192096624650/stars.png">
+                            <img class="restaurant-score" src="https://cdn.discordapp.com/attachments/206957264408412162/630042617047941132/image_1.png">
                             <p class="price"><i class="fas fa-shopping-basket"></i> Min. €11,00</p>
                             <p class="time"><i class="far fa-clock"></i> 30 min</p>
+                            <p class="tags">Sushi, soep, vis</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-sm-6 restaurant-grid-item">
+                <div class="col col-12 col-sm-6 col-lg-4 restaurant-grid-item " onclick="document.location='/#';return false;">
                     <div class="restaurant-card" style="background-image:url('https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-957724994-1558091653.jpg?crop=0.670xw:1.00xh;0.167xw,0&resize=640:*');background-position:center;">
                         <div class="restaurant-name">
                             <p><i class="far fa-star" aria-hidden="true"></i> Tomo Sushi</p>
                         </div>
+                        <div class="restaurant-status">
+                            <div class="status restaurant-status-closed">
+                                <p>Gesloten</p>
+                            </div>
+                        </div>
                         <div class="restaurant-info">
-                            <img class="restaurant-score" src="https://cdn.discordapp.com/attachments/523130323618234418/629730192096624650/stars.png">
+                            <img class="restaurant-score" src="https://cdn.discordapp.com/attachments/206957264408412162/630042617047941132/image_1.png">
                             <p class="price"><i class="fas fa-shopping-basket"></i> Min. €15,00</p>
                             <p class="time"><i class="far fa-clock"></i> 25 min</p>
+                            <p class="tags">Sushi, soep, vis</p>
                         </div>
                     </div>
                 </div>
