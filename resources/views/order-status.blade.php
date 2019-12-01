159f4cb466df87fab2e0198b8add490c57822914
agrida2 <azzeddine_agrida@hotmail.com>
2019-12-01 22:11:20 +0100

    Bestelling page

--- resources/views/order-status.blade.php
+++ resources/views/order-status.blade.php
@@ -1,8 +1,28 @@
 @include('include.navbar')
 
-<body class= "circle-body">
+<div id="restaurant-banner" class="restaurant-banner" style="background-image: url('{{URL('/images/restaurant-banner.png')}}');">
+    
+</div>
+
+<div class= "circle-div">
         
 <div class= "circle-container">
+<div class= "card">
+      <div class= "box">
+          <div class= "percent">
+              <svg> 
+                  <circle cx="70" cy= "70" r="70"></circle>
+                  <circle cx="70" cy= "70" r="70"></circle>
+              </svg>
+              <div class= "number">
+                    <h2><i class="fas fa-check-circle"></i></h2>
+              </div>
+            </div>
+            <h2 class="text">Bestelling </h2>
+            <h2 class="text1">bevestigd!</h2>
+            
+          </div> 
+    </div>
     <div class= "card">
       <div class= "box">
           <div class= "percent">
@@ -11,13 +31,26 @@
                   <circle cx="70" cy= "70" r="70"></circle>
               </svg>
               <div class= "number">
-                    <h2>90 <span>%</span></h2>
+                    <h2>90 <span>Minuten</span></h2>
               </div>
             </div>
-            <h2 class="text">Status</h2>
+            <h2 class="text">Bezorging over</h2>
           </div>
           
-      </div>  
-    </div>
+      </div> 
+
+    <div class= "card">
+      <div class= "box">
+          <div class= "percent">
+              <svg> 
+                  <circle cx="70" cy= "70" r="70"></circle>
+                  <circle cx="70" cy= "70" r="70"></circle>
+              </svg>
+              <div class= "number">
+                    <h2><i class='fas fa-star'></i></h2>
+              </div>
+            </div>
+            <h2 class="text">Beoordelen?</h2>
+          </div>
     
-</body>
+</div>
